9a7d43dc902995005dfc101eb5a5add152b01cb6
appdevgenie <appdevgenie@gmail.com>
2020-08-30 09:27:34 +0200

    change theme remove action bar

--- app/src/main/java/com/appdevgenie/practiceproject/MainActivity.java
+++ app/src/main/java/com/appdevgenie/practiceproject/MainActivity.java
@@ -37,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
 
         TabLayout tabs = findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
-        FloatingActionButton fab = findViewById(R.id.fab);
+       /* FloatingActionButton fab = findViewById(R.id.fab);
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
 
 
     }
