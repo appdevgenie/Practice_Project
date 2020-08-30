ea8f2e581b2239974b10e92834ff0876babcc619
appdevgenie <appdevgenie@gmail.com>
2020-08-30 11:46:29 +0200

    add post result dialog box

--- app/src/main/java/com/appdevgenie/practiceproject/ProjectSubmissionActivity.java
+++ app/src/main/java/com/appdevgenie/practiceproject/ProjectSubmissionActivity.java
@@ -3,13 +3,18 @@ package com.appdevgenie.practiceproject;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class ProjectSubmissionActivity extends AppCompatActivity implements View.OnClickListener {
@@ -19,11 +24,15 @@ public class ProjectSubmissionActivity extends AppCompatActivity implements View
     public static final String EMAIL_ADDRESS = "appdevgenie@gmail.com";
     public static final String GITHUB_LINK = "https://github.com/appdevgenie/Practice_Project";
 
+    private Context context;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_submission);
+
+        context = getApplicationContext();
 
         ImageButton imageButtonBack = findViewById(R.id.image_button_back);
         imageButtonBack.setOnClickListener(this);
@@ -72,8 +81,15 @@ public class ProjectSubmissionActivity extends AppCompatActivity implements View
             @Override
             public void onClick(View view) {
                 //TODO: POST info with retrofit
-                Toast.makeText(getApplicationContext(), "Posting info", Toast.LENGTH_SHORT).show();
+                //Toast.makeText(getApplicationContext(), "Posting info", Toast.LENGTH_SHORT).show();
+
                 alertDialog.dismiss();
+
+                //success
+                //showResultDialog(R.drawable.ic_tick_green, "Submission successful");
+
+                //unsuccessful
+                //showResultDialog(R.drawable.ic_warning_red, "Submission not successful");
             }
         });
         buttonCancel.setOnClickListener(new View.OnClickListener() {
@@ -82,5 +98,21 @@ public class ProjectSubmissionActivity extends AppCompatActivity implements View
                 alertDialog.dismiss();
             }
         });
+    }
+
+    private void showResultDialog(int imageResource, String message){
+
+        Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.dialog_submit_result);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        TextView tvDialog = dialog.findViewById(R.id.text_view_result);
+        tvDialog.setText(message);
+
+        ImageView imageView = dialog.findViewById(R.id.image_view_result);
+        imageView.setImageResource(imageResource);
+
+        dialog.show();
     }
 }
